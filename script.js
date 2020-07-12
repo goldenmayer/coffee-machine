7839d01d88ddce69d1a18adbf34471f881bd913d
123KROTLQfe <goldenmayery@yandex.ru>
2020-07-12 12:40:58 +0300

    Добавили купюры

--- script.js
+++ script.js
@@ -83,3 +83,117 @@ function changeDisplayText(text) {
   let displayText = document.querySelector(".display span"); // локальная переменная для нашей функции
   displayText.innerHTML = text;
 }
+
+/*-----------Drag'n'Drop---------------- - */
+
+let money = document.querySelectorAll(".money img"); // ищем все купюры
+
+/*for (let i = 0; i < money.length; i++) {    //прогоняем купюры через цикл    вариант 1 
+  money[i].onclick = takeMoney;
+} */
+
+/*<div class="coffee-item" onclick="cookCoffee('Капучино', 92, this)"></div>
+coffeeItem.onclick = function () {
+  cookCoffee('Капучино', 92, this);
+}*/
+// В функцию, которая присвоена событию, передается this, который возвращает элемент, на котором это событие совершено
+
+for (let bill of money) {   // вариант 2 bill - купюра 
+  bill.onmousedown = takeMoney;
+}
+// В функцию, которая присвоена событию, первым параметром передается объект события - event
+function takeMoney(event) {
+  event.preventDefault(); // убирает призрака купюры
+/*  console.log(this);
+  console.log(event);
+  console.log([event.target, event.clientX, event.clirntY]);*/
+  let bill = this;
+  
+/*  console.log(bill.style.height);// ""
+  console.log(bill.style.width);// ""
+  console.log( bill.getBoundingClientRect() );// возвращает объект domRect в пикселях*/
+
+  let billCoords = bill.getBoundingClientRect();  // getBoundingClientRect - получение объектаа, который описывает положение элемента на странице
+  
+  let billHeight = billCoords.height;
+  let billWidth = billCoords.width;
+  
+  bill.style.position = "absolute";
+  if (!bill.style.transform) { //bill.style.transform == "" ("" == false)
+    bill.style.top = (event.clientY - billHeight/2) + "px";
+    bill.style.left = (event.clientX - billWidth/2) + "px";
+    bill.style.transform = "rotate(90deg)";
+  } else {
+    bill.style.top = (event.clientY - billWidth/2) + "px";
+    bill.style.left = (event.clientX - billHeight/2) + "px";
+  }
+ bill.style.transition = "transform .3s)";
+  
+  window.onmousemove = function(event) {
+                                                        //console.log([event.clientX, event.clirntY]);
+                                                        //console.log(this);
+    let billCoords = bill.getBoundingClientRect();
+    let billHeight = billCoords.height;
+    let billWidth = billCoords.width;
+    bill.style.top = (event.clientY - billWidth/2) + "px";
+    bill.style.left = (event.clientX - billHeight/2) + "px";  
+  };
+  
+  bill.onmouseup = function() {
+    window.onmousemove = null;
+    if ( inAtm(bill) ) {
+      console.log( bill.getAttribute("data-cost") );
+      console.log( bill.dataset.cost );
+      balance.value = +balance.value + +bill.dataset.cost; // для сложения баланса на экране делаем: приведение к числу balance.value , слодение  и приведение кчислу bill.dataset.cost
+      bill.remove(); // удаляем элемент , т.о. при попадании в банкомат купюра исчезает
+    }
+  };
+}
+
+function inAtm(bill) {
+  let atm = document.querySelector(".atm img");
+  
+  let atmCoords = atm.getBoundingClientRect();
+  let atmLeftX = atmCoords.x;
+  let atmRightX = atmCoords.x + atmCoords.width;
+  let atmTopY = atmCoords.y;
+  let atmBottomY = atmCoords.y + atmCoords.height/3;
+  
+  let billCoords = bill.getBoundingClientRect();
+  let billLeftX = billCoords.x;
+  let billRightX = billCoords.x + billCoords.width;
+  let billY = billCoords.y;
+  if (
+        billLeftX > atmLeftX
+    &&  billRightX < atmRightX
+    &&  billY > atmTopY
+    &&  billY < atmBottomY
+    ) {
+    return true;
+    } else {
+      return false;
+    }
+  }
+
+/*  return {
+    atm: [atmLeftX, atmRightX, atmTopY, atmBottomY],
+    bill: [billLeftX, billRightX, billY],
+  };*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
